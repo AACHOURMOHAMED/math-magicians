6b9aa2682c2959ca11c32b38bff5039a413c5899
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-08-03 22:11:26 +0100

    Add logic and events to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,59 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+/* eslint-disable no-useless-constructor */
+import React from 'react';
 import Button from './button/Button';
 import './Calculator.scss';
+import Result from './result';
+import calculate from '../logic/calculate';
 
-export class Calculator extends Component {
+export class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { output: '0' };
+    this.obj = {};
+  }
+
+  handleButtonClick = (buttonName) => {
+    this.obj = calculate(this.obj, buttonName);
+
+    if (this.obj.operation && !this.obj.next) {
+      this.setState({ output: this.obj.operation });
+    } else if (this.obj.next) {
+      this.setState({ output: this.obj.next });
+    } else if (this.obj.total) {
+      this.setState({ output: this.obj.total });
+    } else {
+      this.setState({ output: 0 });
+    }
+  }
+
   render() {
+    const { output } = this.state;
     return (
       <main className="Hero">
         <div className="container">
           <div className="buttons">
-            <span>0</span>
-            <div className="text" />
-            <Button>AC</Button>
-            <Button>+/-</Button>
-            <Button>%</Button>
-            <Button buttonStyle="btn--sec">÷</Button>
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button buttonStyle="btn--sec">x</Button>
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button buttonStyle="btn--sec">-</Button>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button buttonStyle="btn--sec">+</Button>
-            <Button buttonStyle="btn--zero">0</Button>
-            <Button>.</Button>
-            <Button buttonStyle="btn--sec">=</Button>
+            <Result output={output} />
+            <Button handleButtonClick={this.handleButtonClick}>AC</Button>
+            <Button handleButtonClick={this.handleButtonClick}>+/-</Button>
+            <Button handleButtonClick={this.handleButtonClick}>%</Button>
+            <Button buttonStyle="btn--sec" handleButtonClick={this.handleButtonClick}>÷</Button>
+            <Button handleButtonClick={this.handleButtonClick}>7</Button>
+            <Button handleButtonClick={this.handleButtonClick}>8</Button>
+            <Button handleButtonClick={this.handleButtonClick}>9</Button>
+            <Button buttonStyle="btn--sec" handleButtonClick={this.handleButtonClick}>x</Button>
+            <Button handleButtonClick={this.handleButtonClick}>4</Button>
+            <Button handleButtonClick={this.handleButtonClick}>5</Button>
+            <Button handleButtonClick={this.handleButtonClick}>6</Button>
+            <Button buttonStyle="btn--sec" handleButtonClick={this.handleButtonClick}>-</Button>
+            <Button handleButtonClick={this.handleButtonClick}>1</Button>
+            <Button handleButtonClick={this.handleButtonClick}>2</Button>
+            <Button handleButtonClick={this.handleButtonClick}>3</Button>
+            <Button buttonStyle="btn--sec" handleButtonClick={this.handleButtonClick}>+</Button>
+            <Button buttonStyle="btn--zero" handleButtonClick={this.handleButtonClick}>0</Button>
+            <Button handleButtonClick={this.handleButtonClick}>.</Button>
+            <Button buttonStyle="btn--sec" handleButtonClick={this.handleButtonClick}>=</Button>
           </div>
         </div>
       </main>
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,14 +4,23 @@ import React, { Component } from 'react';
 import './button.scss';
 
 export class button extends Component {
+  constructor(props) {
+    super(props);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleButtonClick(e) {
+    const { handleButtonClick } = this.props;
+    handleButtonClick(e.target.textContent);
+  }
+
   render() {
-    const { children, buttonStyle, onClick } = this.props;
+    const { children, buttonStyle } = this.props;
     const STYLES = ['btn--primary', 'btn--sec', 'btn--zero'];
     const CheckBtnStyles = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     return (
-      <div>
-        <button className={`btn ${CheckBtnStyles}`} onClick={onClick} value={children} type="button">{children}</button>
-      </div>
+      <button className={`btn ${CheckBtnStyles}`} onClick={this.handleButtonClick} type="button">{children}</button>
     );
   }
 }
